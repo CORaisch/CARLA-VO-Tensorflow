babce10301d373f20067369d741a09c02d8671cd
CORaisch <claudio.raisch@hotmail.de>
2019-10-15 21:49:01 +0200

    updated gitignore and reworked clear_dir script

--- clear_dir.sh
+++ clear_dir.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# clear csv logs
 if [ -d "logs/csv" ]; then
     rm -rf logs/csv/*
 else
     mkdir -p logs/csv
 fi
 
+# clear tensorboard logs
 if [ -d "logs/tensorboard" ]; then
     rm -rf logs/tensorboard/*
 else
     mkdir -p logs/tensorboard
+fi
+
+# clear checkpoints
+if [ -d "checkpoints" ]; then
+    rm -rf checkpoints/*
+else
+    mkdir checkpoints
 fi
