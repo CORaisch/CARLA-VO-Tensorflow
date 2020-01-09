0f602b0f39f8df37255200b77362ea7d169ba282
CORaisch <claudio.raisch@hotmail.de>
2020-01-09 13:30:05 +0100

    implemented simple conv net for testing on bigger images

---
+++ src/model_build_scripts/model__simple_conv2d.py
@@ -0,0 +1,81 @@
+# script creates simple flat dummy model that takes arbitrary input arguments and has 6 output neurons
+
+import tensorflow as tf
+import os, zipfile, argparse
+import src.config as config
+
+def parse_args():
+    argparser = argparse.ArgumentParser(description="This scripts generates a simple network for debug/test usages.")
+    argparser.add_argument('config', help="same config file as used for training with 'train_sequence.py'. Config file is required to set the input layers names as 'train_tfrec.py' expects it. Names will be generated from 'TRAINING_FILES' option, so ensure to use this option consistently.")
+    argparser.add_argument('--model_out', '-out', default='', help="path to where the model should be saved.")
+    return argparser.parse_args()
+
+# NOTE 'arch_file' holds the path to a sequence zip archive
+# NOTE 't_inputs' defines the number of timesteps presented the DNN as input
+#       -> recording N images at a single point in time then setting 't_inputs' to M will result in N*M input images to the DNN
+def make_layernames(arch_file, t_inputs):
+    layernames = []
+    ## get filenames from archive
+    # open archive
+    fz = zipfile.ZipFile(arch_file, 'r')
+    # extract names of image files
+    for f in fz.namelist():
+        # skip labels file
+        if f == 'labels.npz':
+            continue
+        # cut away the file extension
+        name = f.split('.')[0]
+        # create enumerate input layernames
+        for t in range(t_inputs):
+            layernames.append(name + '_' + str(t))
+    # close archive
+    fz.close()
+    ## return list of input layernames
+    return layernames
+
+# NOTE 'layernames' holds the names of the input layers => size of 'layernames' == size of input layers
+# NOTE 'image_shape' is tuple that holds resolution of input images, assuming all images to have the same size
+def create_model(layernames, image_shape):
+    # generate input layers
+    input_layers   = []
+    flatten_layers = []
+    for layername in layernames:
+        input_layer   = tf.keras.layers.Input(shape=image_shape, name=layername)
+        input_layers.append(input_layer)
+    # stack inputs at channel dimension of images
+    input_stacked = tf.keras.layers.concatenate(inputs=input_layers, axis=-1, name='stack_inputs')
+    # add convolution layers
+    conv1 = tf.keras.layers.Conv2D(1024, 7, padding='same', strides=(16,64), activation='relu', data_format='channels_last', name='conv1')(input_stacked)
+    conv2 = tf.keras.layers.Conv2D(512, 7, padding='same', strides=8, activation='relu', data_format='channels_last', name='conv2')(conv1)
+    conv3 = tf.keras.layers.Conv2D(64, 7, padding='same', strides=2, activation='relu', data_format='channels_last', name='conv3')(conv2)
+    # flatten output tensor
+    flatten = tf.keras.layers.Flatten(data_format='channels_last', name='flatten')(conv3)
+    # make mlp with 6 outpus for the 6 dof poses
+    mid = tf.keras.layers.Dense(500, activation='relu', name='dense1')(flatten)
+    out = tf.keras.layers.Dense(6, activation='linear', name='dense2')(mid)
+    # set and compile model
+    model = tf.keras.models.Model(inputs=input_layers, outputs=out)
+    # return compiled model
+    return model
+
+def main():
+    # parse input arguemts
+    args = parse_args()
+    conf = config.Config(args.config)
+    # make header for the DNN
+    layernames = make_layernames(conf.training_files[0], conf.input_timesteps)
+    # create and compile model
+    print("[INFO] information about generated DNN model:")
+    model = create_model(layernames, conf.training_shape)
+    model.summary(line_length=150)
+    # write model to disk
+    name = 'simple_conv__in' + str(len(layernames)) \
+             + '_tInputs'     + str(conf.input_timesteps) \
+             + '_imw'        + str(conf.training_shape[0]) \
+             + '_imh'        + str(conf.training_shape[1]) \
+             + '_imc'        + str(conf.training_shape[2]) \
+             + '_out6'       + '.h5'
+    model.save(os.path.join(args.model_out, name))
+
+if __name__=="__main__":
+    main()
